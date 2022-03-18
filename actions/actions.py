117667da961658e1776ad5cb15de5bdf085a1f2f
SeyramDiaba <stephen.diaba@amalitech.org>
2022-03-18 03:07:25 +0000

    first slot and actions.py update

--- actions/actions.py
+++ actions/actions.py
@@ -13,16 +13,22 @@ from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 #
 #
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+class ActionHelloWorld(Action):
+
+    def name(self) -> Text:
+        return "action_hello_world"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        terms = tracker.get_slot('terms')
+
+        dispatcher.utter_message(text='you wanted to know the full meaning of: '+ str(terms))
+
+
+
+        #dispatcher.utter_message(text="Hello World!")
+
+        return []
 
