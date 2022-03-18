5b049a8d7680dbef1110f54a85f63c8fdf0e417e
Maame Esi Baidoo <maame.baidoo@amalitech.org>
2022-03-18 13:04:58 +0000

    Terms

--- actions/actions.py
+++ actions/actions.py
@@ -11,18 +11,21 @@ from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from terms import Terms
+
+
+class TermsAndDefinitions(Action):
+
+    def name(self) -> Text:
+        return "action_term_api"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        term = tracker.latest_message['text']
+        definition = str(Terms(term))
+        dispatcher.utter_message("utter_definition", tracker, term = term, definition = definition)
+
+        return []
 
