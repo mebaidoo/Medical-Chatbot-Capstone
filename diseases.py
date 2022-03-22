36b7f98f1931e7f0e8bae4cdb37bc7388973978e
SeyramDiaba <stephen.diaba@amalitech.org>
2022-03-22 07:36:22 +0000

    disease actions

--- diseases.py
+++ diseases.py
@@ -1,8 +1,12 @@
 import pandas as pd
 
+def disease_repo():
+    # pd.set_option('display.max_rows', None)
+    df = pd.read_csv("df_diseases.csv")
+    df= df[["name","symptoms","treatment"]]
+    #print(df)
+    return df
 
-# pd.set_option('display.max_rows', None)
-df = pd.read_csv("df_diseases.csv")
-df= df[["name","symptoms","treatment"]]
-#print(df)
+print(disease_repo())
+
 
