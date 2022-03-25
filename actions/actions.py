b2345a573ddaeed28689ac6b3b985ff30304f4c1
SeyramDiaba <stephen.diaba@amalitech.org>
2022-03-25 10:18:53 +0000

    Fallback Policy Setup

--- actions/actions.py
+++ actions/actions.py
@@ -129,3 +129,17 @@ class MapLocations(Action):
             dispatcher.utter_message(response="utter_correct_name")
 
         return []
+
+# Fallback action
+class MyFallback(Action):
+
+    def name(self) -> Text:
+        return "action_my_fallback"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        dispatcher.utter_message(template="utter_fallback")
+
+        return []
